73795f8952af92ab418f282745a95ce0d6401784
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-11-22 14:54:03 +0900

    複数行のコメントは/**/を使う

--- java練習/lesson2.java
+++ java練習/lesson2.java
@@ -1,8 +1,12 @@
 package java練習;
+
 /*クラスのpoint
 クラス名とファイル名は合わせる
 処理のまとまりのことをメソッドと呼び、javaはmainメソッドから実行される。
 */
 public class lesson2 {
+  public static void main(String[] ages) {
+    System.out.println("複数のコメントアウトは/**/を使う");
+  }
 
 }
