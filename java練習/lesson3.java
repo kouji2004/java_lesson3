09b418a3ca1450a9413420cb73dc95c190baf942
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-11-22 15:13:03 +0900

    変数の使い方宣言、代入、参照

---
+++ java練習/lesson3.java
@@ -0,0 +1,17 @@
+package java練習;
+
+public class lesson3 {
+  /*
+   * 変数
+   * 変数はデータの入れ物
+   * 変数の使い方→宣言、代入、参照
+   * 宣言と代入を同時に使うことを初期化と言う
+   */
+  public static void main(String[] ages) {
+    int month;
+    month = 11; //
+    int day = 22; //
+    System.out.println("こんにちは");
+    System.out.println("今日は" + month + day + "です"); // 変数名(monthとday)を参照している
+  }
+}
