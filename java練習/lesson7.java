a85204a85073c2228073c2a8f1429adcca9398fc
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-11-23 23:58:11 +0900

    クラス

--- java練習/lesson7.java
+++ java練習/lesson7.java
@@ -8,11 +8,19 @@ public class lesson7 {
     System.out.println("args.length;" + args.length);
   }
   /*
-   * 処理実行 java lesson7.java hello java
+   * 処理実行⓵ java lesson7.java hello java
    * 結果
    * args[0];hello
    * args[0];java
    * args.length;2
+   */
+
+  /*
+   * 処理実行⓶ java lesson7.java hello java lesson
+   * 結果
+   * args[0];hello
+   * args[0];java
+   * args.length;3 ←ここに追加される
    */
 
 }
