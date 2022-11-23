a34a45930022e03ba7b167ae2c61e56d3b15a29b
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-11-24 00:17:55 +0900

    演習問題

--- java練習/lesson8.java
+++ java練習/lesson8.java
@@ -6,4 +6,23 @@ public class lesson8 {
     System.out.println("こんんちは");
     System.out.println("頑張りましょう");
   }
+
+  class lesson1 {
+    // 演習問題2
+    public static void main(String[] ages) {
+      // 変数の宣言と値の代入
+      int num = 10;
+      double pi = 3.14;
+      // char blood;
+      boolean fun = true;
+      String msg = "こんんちは";
+
+      // 値の表示
+      System.out.println(num);
+      System.out.println(pi);
+      System.out.println(msg);
+      System.out.println(pi);
+      // System.out.println(blood);
+    }
+  }
 }
