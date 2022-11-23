347e22bfae587d5aec13e1cd571ae23cdc68baaf
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-11-24 00:26:18 +0900

    演習問題

--- java練習/lesson8.java
+++ java練習/lesson8.java
@@ -1,28 +1,48 @@
 package java練習;
 
+class lesson2 {
+  public static void main(String[] ages) {
+    String[] name = new String[5];
+
+    name[0] = "1";
+    name[1] = "2";
+    name[2] = "3";
+    name[3] = "4";
+    name[4] = "5";
+
+    System.out.println(name[0]);
+    System.out.println(name[1]);
+    System.out.println(name[2]);
+    System.out.println(name[3]);
+    System.out.println(name[4]);
+  }
+}
+
+class lesson1 {
+  // 演習問題2
+  public static void main(String[] ages) {
+    // 変数の宣言と値の代入
+    int num = 10;
+    double pi = 3.14;
+    char blood = 'A';
+    boolean fun = true;
+    String msg = "こんんちは";
+
+    // 値の表示
+    System.out.println(num);
+    System.out.println(pi);
+    System.out.println(msg);
+    System.out.println(pi);
+    System.out.println(blood);
+  }
+}
+
 public class lesson8 {
   // 演習問題1
   public static void main(String[] ages) {
     System.out.println("こんんちは");
     System.out.println("頑張りましょう");
+
   }
 
-  class lesson1 {
-    // 演習問題2
-    public static void main(String[] ages) {
-      // 変数の宣言と値の代入
-      int num = 10;
-      double pi = 3.14;
-      char blood = 'A';
-      boolean fun = true;
-      String msg = "こんんちは";
-
-      // 値の表示
-      System.out.println(num);
-      System.out.println(pi);
-      System.out.println(msg);
-      System.out.println(pi);
-      System.out.println(blood);
-    }
-  }
 }
