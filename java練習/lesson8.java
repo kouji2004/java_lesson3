d1d3476c786847b35714cd2b2c189b456426e5c8
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-11-24 00:02:19 +0900

    演習問題

--- java練習/lesson8.java
+++ java練習/lesson8.java
@@ -1,5 +1,9 @@
 package java練習;
 
 public class lesson8 {
-  // 演習問題
+  // 演習問題1
+  public static void main(String[] ages) {
+    System.out.println("こんんちは");
+    System.out.println("頑張りましょう");
+  }
 }
