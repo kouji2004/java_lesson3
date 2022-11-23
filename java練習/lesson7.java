71abdd56b4b2e5471468c55261b38be73f25da2d
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-11-23 23:54:56 +0900

    クラス

--- java練習/lesson7.java
+++ java練習/lesson7.java
@@ -1,11 +1,9 @@
 package java練習;
 
 public class lesson7 {
-  class CmdLine {
-    public static void main(String[] args) {
-      System.out.println("args[0];" + args[0]);
-      System.out.println("args[0];" + args[1]);
-      System.out.println("args.length;" + args.length);
-    }
+  public static void main(String[] args) {
+    System.out.println("args[0];" + args[0]);
+    System.out.println("args[0];" + args[1]);
+    System.out.println("args.length;" + args.length);
   }
 }
