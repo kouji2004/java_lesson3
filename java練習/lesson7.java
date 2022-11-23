705dc8f924122c5460ede8f94f8d99cb8327bb96
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-11-23 23:57:16 +0900

    クラス

--- java練習/lesson7.java
+++ java練習/lesson7.java
@@ -3,7 +3,16 @@ package java練習;
 public class lesson7 {
   public static void main(String[] args) {
     System.out.println("args[0];" + args[0]);
-    System.out.println("args[0];" + args[1]);
+    System.out.println("args[1];" + args[1]);
+    System.out.println("args[2];" + args[2]);
     System.out.println("args.length;" + args.length);
   }
+  /*
+   * 処理実行 java lesson7.java hello java
+   * 結果
+   * args[0];hello
+   * args[0];java
+   * args.length;2
+   */
+
 }
