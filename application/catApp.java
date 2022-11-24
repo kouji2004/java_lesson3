70065d86ae9eed2536ebb20bde9d6dfbfd0c5166
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-11-24 18:36:13 +0900

    %s,%d,%f【フォーマット指定子

--- application/catApp.java
+++ application/catApp.java
@@ -13,9 +13,14 @@ class Cat {
   int intimacy = 0; // 親密度 親密度に関しては最初は0なので、ここで明示しておく
 
   // コンストラクタを考える
+  // コンストラクタとは、クラスのインスタンス生成時に実行される処理のこと
   Cat(String name, String type) {
     this.name = name;
     this.type = type;
   }
 
+  // 自分自身の情報を文字列情報として表せるメソッドを作成
+  String showStatus() {
+    return String.format("%s[%s](%d)", this.name, this.type, this.intimacy);
+  }
 }
