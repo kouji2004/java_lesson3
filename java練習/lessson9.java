29c52931513f2a4cb8b27cc9b2cf250a9fadaa91
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-11-24 15:24:01 +0900

    苦手な範囲を復習する

--- java練習/lessson9.java
+++ java練習/lessson9.java
@@ -1,7 +1,24 @@
 package java練習;
 
+class lesson2 {
+  // 苦手な範囲を復習する(多次元配列)
+  public static void main(String[] ages) {
+    int[][] name = new int[2][2]; // 行列
+
+    name[0][0] = 11;
+    name[0][1] = 12;
+    name[1][0] = 13;
+    name[1][1] = 14;
+
+    System.out.println(name[0][0]);
+    System.out.println(name[0][1]);
+    System.out.println(name[1][0]);
+    System.out.println(name[1][1]);
+  }
+}
+
 class lesson1 {
-  // 苦手な範囲を復習する
+  // 苦手な範囲を復習する(配列)
   public static void main(String[] ages) {
 
     // 配列の生成
