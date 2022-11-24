7c84e016a936f59e602b006588ec96d9f2387663
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-11-24 18:30:27 +0900

    コンストラクタを考える

--- application/catApp.java
+++ application/catApp.java
@@ -10,5 +10,12 @@ public class catApp {
 class Cat {
   String name; // 猫の名前
   String type; // 猫の種類
-  int intimacy = 0; // 親密度
+  int intimacy = 0; // 親密度 親密度に関しては最初は0なので、ここで明示しておく
+
+  // コンストラクタを考える
+  Cat(String name, String type) {
+    this.name = name;
+    this.type = type;
+  }
+
 }
