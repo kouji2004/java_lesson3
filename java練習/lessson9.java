901e6d8e385f92df01415b9db9e3bd00c65e15d1
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-11-24 15:16:06 +0900

    苦手な範囲を復習する

---
+++ java練習/lessson9.java
@@ -0,0 +1,19 @@
+package java練習;
+
+class lesson1 {
+  // 苦手な範囲を復習する
+  public static void main(String[] ages) {
+
+    // 配列の生成
+    int[] name = new int[4];
+
+    name[0] = 1;
+    name[1] = 2;
+
+    System.out.println(name[0]);
+    System.out.println(name[1]);
+  }
+}
+
+public class lessson9 {
+}
