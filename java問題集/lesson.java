0eef823b891c22b532e167292798d98cb948c9f9
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-11-25 18:31:09 +0900

    java問題集

--- java問題集/lesson.java
+++ java問題集/lesson.java
@@ -1,5 +1,18 @@
 package java問題集;
 
+class lesson2 {
+  // xに10を代入し、それに５を３回足し結果を出力せよ。(whileと+=を使うこと)
+  public static void main(String[] ages) {
+    int x = 10;
+    int count = 3;
+    while (count > 0) {
+      x += 5;
+      count--;
+    }
+    System.out.println("Ans:" + x);
+  }
+}
+
 class lesson1 {
   /*
    * xに23,yに6を代入して以下の結果を求め出力せよ。
