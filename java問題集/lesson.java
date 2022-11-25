2354becdfab477308ec7dac73667bc53ded618ba
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-11-25 18:42:44 +0900

    java問題集

--- java問題集/lesson.java
+++ java問題集/lesson.java
@@ -2,6 +2,19 @@ package java問題集;
 
 import java.util.Random;
 
+class lesson4 {
+  /*
+   * bottomに4.3を代入、heightに5.4を代入して、三角形の面積を求めよ。
+   * [実行例]
+   * 底辺:4.3,高さ:5.4の三角形の面積は:11.61
+   */
+  public static void main(String[] ages) {
+    double bottom = 4.3;
+    double height = 5.4;
+    System.out.println(bottom * height / 2);
+  }
+}
+
 class lesson3 {
   // 1~10の乱数を生成して、それが偶数か奇数かを判定
   public static void main(String[] ages) {
