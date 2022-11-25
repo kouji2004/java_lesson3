3fc46c8f72386de8f603c881b1bc2fdf6b0e5677
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-11-25 18:25:54 +0900

    java問題集

---
+++ java問題集/lesson.java
@@ -0,0 +1,30 @@
+package java問題集;
+
+class lesson1 {
+  /*
+   * xに23,yに6を代入して以下の結果を求め出力せよ。
+   * [実行例]
+   * x+y=29
+   * x-y=17
+   * x*y=138
+   * x/y=3
+   * x%y=5
+   */
+  public static void main(String[] ages) {
+    int x;
+    int y;
+
+    x = 23;
+    y = 6;
+
+    System.out.println(x + y);
+    System.out.println(x - y);
+    System.out.println(x * y);
+    System.out.println(x / y);
+    System.out.println(x % y);
+  }
+}
+
+public class lesson {
+
+}
