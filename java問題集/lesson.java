aaf716d2ee9109fdcdcdfb8a7128bd87e5bba33a
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-11-25 18:37:40 +0900

    java問題集

--- java問題集/lesson.java
+++ java問題集/lesson.java
@@ -1,5 +1,19 @@
 package java問題集;
 
+import java.util.Random;
+
+class lesson3 {
+  // 1~10の乱数を生成して、それが偶数か奇数かを判定
+  public static void main(String[] ages) {
+    int num = new java.util.Random().nextInt(10) + 1;
+    if (num % 2 == 0) {
+      System.out.println("偶数" + num);
+    } else {
+      System.out.println("奇数" + num);
+    }
+  }
+}
+
 class lesson2 {
   // xに10を代入し、それに５を３回足し結果を出力せよ。(whileと+=を使うこと)
   public static void main(String[] ages) {
