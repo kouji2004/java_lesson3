35fc5900e261b5e531fc052c53b8c27f49cb464b
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-11-27 06:13:41 +0900

    1~100までの和を求めよ。

---
+++ java練習2/lesson5.java
@@ -0,0 +1,14 @@
+package java練習2;
+
+public class lesson5 {
+  /*
+   * 1~100までの和を求めよ。
+   */
+  public static void main(String[] ages) {
+    int sum = 0;
+    for (int i = 1; i <= 100; i++) {
+      sum += i;
+    }
+    System.out.println(sum);
+  }
+}
