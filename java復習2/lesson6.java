e2e71d1b3dd0c5f14ce847c90a53560271a00f6e
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-12-10 05:19:06 +0900

    １０月を英語で何? equalsとは?

--- java復習2/lesson6.java
+++ java復習2/lesson6.java
@@ -1,5 +1,12 @@
 package java復習2;
 
+/*
+ * Java の equals () メソッドは 2つの値を比較し、
+ * true または false のブール値を返します。
+ * このメソッドを使用して、列挙値を比較できます。
+ * ここでは、 Color 列挙型を使用してその値を比較しました。
+ *  最初の値は false を返しますが、2 番目の値は true を返します
+*/
 public class lesson6 {
   public static void main(String[] ages) {
     System.out.println("10月を英語で何?");
