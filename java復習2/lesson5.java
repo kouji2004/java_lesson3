462eba93a4eb27e0c06412fcacf8c3c969c4145b
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-12-09 18:29:20 +0900

    結婚しているか

--- java復習2/lesson5.java
+++ java復習2/lesson5.java
@@ -1,5 +1,12 @@
 package java復習2;
 
+/*
+ * 結婚しているかどうかを管理する変数isMarriedにtrueを代入する。
+isMarriedがtrueならば「結婚しています。」
+falseならば「結婚していません。」と出力する。
+[実行例]
+結婚しています。
+*/
 public class lesson5 {
   public static void main(String[] ages) {
     boolean num = true;
