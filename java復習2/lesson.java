8950c9a96799ca3e13f8b8c78e9300f816deba9c
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-12-09 16:36:05 +0900

    while文

---
+++ java復習2/lesson.java
@@ -0,0 +1,13 @@
+package java復習2;
+
+public class lesson {
+  public static void main(String[] ages) {
+    int x = 10;
+    int count = 3;
+    while (count > 0) {
+      x += 5;
+      count--;
+    }
+    System.out.println("ans:" + x);
+  }
+}
