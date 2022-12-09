979ef1e9a989bcc16f5499f9f4cdb49f7d4b7dff
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-12-09 17:03:45 +0900

    名前を入力させ、それを出力する

---
+++ java復習2/lesson4.java
@@ -0,0 +1,9 @@
+package java復習2;
+
+public class lesson4 {
+  public static void main(String[] ages) {
+    System.out.println("名前を入力してください");
+    String name = new java.util.Scanner(System.in).nextLine();
+    System.out.println("こんにちは" + name + "さん");
+  }
+}
