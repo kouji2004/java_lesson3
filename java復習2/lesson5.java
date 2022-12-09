ef8ad6404fcc2245cd0b9df085369ad1235c0c4b
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-12-09 18:28:56 +0900

    結婚しているか

---
+++ java復習2/lesson5.java
@@ -0,0 +1,12 @@
+package java復習2;
+
+public class lesson5 {
+  public static void main(String[] ages) {
+    boolean num = true;
+    if (num) {
+      System.out.println("いえす");
+    } else {
+      System.out.println("のー");
+    }
+  }
+}
