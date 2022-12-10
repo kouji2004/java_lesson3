cac2e44c689fe48b41d03794e2654dec52ef3433
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-12-10 22:46:20 +0900

    和を求めよ

---
+++ java復習2/lesson10.java
@@ -0,0 +1,12 @@
+package java復習2;
+
+public class lesson10 {
+  public static void main(String[] ages) {
+    int sum = 0;
+    for (int i = 1; i < 10; i++) {
+      sum += i;
+    }
+    System.out.println("1から10まで足すと" + sum + "です");
+
+  }
+}
