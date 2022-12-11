9ac4ec57af8ec2adbd9352a6dc236086f0e1b61c
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-12-11 13:07:57 +0900

    10からカウントダウンし、０になったらStart!!と表示する

---
+++ java復習3/lesson2.java
@@ -0,0 +1,13 @@
+package java復習3;
+
+public class lesson2 {
+  public static void main(String[] ages) {
+    for (int i = 11; i >= 0; i--) {
+      if (i == 0) {
+        System.out.println("stat");
+      } else {
+        System.out.println(i);
+      }
+    }
+  }
+}
