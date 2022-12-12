109a3bc14ff88a27c1a0dc0f5459cfa4c97ad990
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-12-12 19:06:57 +0900

    int型の配列を生成

--- java復習4/lesson.java
+++ java復習4/lesson.java
@@ -6,6 +6,7 @@ public class lesson {
     num[0] = 30;
     num[1] = 70;
     num[2] = 10;
-    System.out.println("n1:"+num[0]+"n2:"+num[1]+"n3:"+num[2]);
+    System.out.println("n1:" + num[0] + "n2:" + num[1] + "n3:" + num[2]);
+
   }
 }
