7390f2ae3d15a192a47b116a39060839bc728b62
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-12-12 18:58:38 +0900

    ★を一つ増えていく

---
+++ java復習3/java14.java
@@ -0,0 +1,12 @@
+package java復習3;
+
+public class java14 {
+  public static void main(String[] ages) {
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j <= i; j++) {
+        System.out.print("★");
+      }
+      System.out.println();
+    }
+  }
+}
