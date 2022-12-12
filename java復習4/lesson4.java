d67f1feca562fb64229faeff6606575b4e40938c
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-12-12 19:23:41 +0900

    char型の配列を宣言すると同時に’H’,’E’,’L’,’L’,’O’,’!’の値で初期化する

---
+++ java復習4/lesson4.java
@@ -0,0 +1,10 @@
+package java復習4;
+
+public class lesson4 {
+  public static void main(String[] ages) {
+    char[] num = { 'H', 'E', 'L', 'L', 'O', '!' };
+    for (int i = 0; i < num.length; i++) {
+      System.out.print(num[i]);
+    }
+  }
+}
