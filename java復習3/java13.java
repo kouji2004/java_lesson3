508570f059e4d7ffd983a7787d6267ced8dd2f7e
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-12-12 18:52:58 +0900

    ★３つ横に表示

---
+++ java復習3/java13.java
@@ -0,0 +1,14 @@
+package java復習3;
+
+public class java13 {
+  public static void main(String[] ages) {
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        for (int g = 0; g < 3; g++) {
+          System.out.print("★");
+        }
+        System.out.println();
+      }
+    }
+  }
+}
