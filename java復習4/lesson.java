a1dd4f3e67d4ef916cd6750b080bee1469654a15
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-12-12 19:06:22 +0900

    int型no

---
+++ java復習4/lesson.java
@@ -0,0 +1,11 @@
+package java復習4;
+
+public class lesson {
+  public static void main(String[] ages) {
+    int[] num = new int[3];
+    num[0] = 30;
+    num[1] = 70;
+    num[2] = 10;
+    System.out.println("n1:"+num[0]+"n2:"+num[1]+"n3:"+num[2]);
+  }
+}
