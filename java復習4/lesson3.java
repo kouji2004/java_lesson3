5b96267e57bdc9da21701fad3b8e29fdb5df0cab
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-12-12 19:17:00 +0900

    doble型の配列生成し上書きする

---
+++ java復習4/lesson3.java
@@ -0,0 +1,9 @@
+package java復習4;
+
+public class lesson3 {
+  public static void main(String[] ages) {
+    double[] nums = { 22.3, 33.1, 44.5 };
+    nums[0] = 22.5;
+    System.out.println("最初の要素を" + nums[0] + "に変更しました。");
+  }
+}
