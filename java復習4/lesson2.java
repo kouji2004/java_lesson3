0f15a2ac24aa5d7c415c6572215e0cec8b5d059e
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-12-12 19:11:40 +0900

    String型の配列を生成2

---
+++ java復習4/lesson2.java
@@ -0,0 +1,14 @@
+package java復習4;
+
+public class lesson2 {
+  public static void main(String[] ages) {
+
+    String[] fruit = new String[3];
+    fruit[0] = "Banana";
+    fruit[1] = "Apple";
+    fruit[2] = "Orange";
+    System.out.println(fruit[0]);
+    System.out.println(fruit[1]);
+    System.out.println(fruit[2]);
+  }
+}
