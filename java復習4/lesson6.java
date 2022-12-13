c1e62770f3a765876e9ef443327be7d686dbb50c
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-12-13 12:39:55 +0900

    int型の配列を要素数5で用意する

---
+++ java復習4/lesson6.java
@@ -0,0 +1,11 @@
+package java復習4;
+
+public class lesson6 {
+  public static void main(String[] ages) {
+    int num = new java.util.Random().nextInt(100);
+    int[] num2 = new int[5];
+    for (int i = 0; i <= num.length; i++) {
+      System.out.println(num2[num]);
+    }
+  }
+}
